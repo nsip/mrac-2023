52aebf2cca2205909128481cc1dee9be042923b7
Qing Miao <cdutwhu@outlook.com>
2023-06-23 16:13:09 +1000

    * clean up old package into /outdated

--- clean.sh
+++ clean.sh
@@ -4,4 +4,7 @@ set -e
 
 find . -type f -executable -exec sh -c "file -i '{}' | grep -q 'x-executable; charset=binary'" \; -print | xargs rm -f
 
+# delete others
+for f in "`find ./ -name '*.log' -or -name '*.temp'`"; do rm "$f"; done
+
 rm -rf ./data-out
